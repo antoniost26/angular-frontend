a766a93154445ff60d522bd2a3f2d30fa2bdc8ff
antoniost26 <91466692+antoniost26@users.noreply.github.com>
2021-12-14 21:55:59 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // API_ROOT: 'https://api.antoniost26.tech',
-  API_ROOT: 'http://127.0.0.1:8000/',
+  API_ROOT: 'https://api.antoniost26.tech',
+  // API_ROOT: 'http://127.0.0.1:8000/',
 };
